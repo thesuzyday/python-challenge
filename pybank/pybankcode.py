a1dd7ebc1f4fde38197ab3bd576dc5ab9260b565
thesuzyday <115379245+thesuzyday@users.noreply.github.com>
2022-11-10 21:47:00 -0600

    Add files via upload
    Updated code

--- pybank/pybankcode.py
+++ pybank/pybankcode.py
@@ -17,7 +17,32 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         print(row)
 
-#Total the net profit/loss
-    sum(row[1],start=0)
-    
+        rowcount=0
+#total number of months (without header)
+    for row in csvreader:
+        rowcount = (rowcount + 1)
+         print("Total months: ", rowcount)
 
+#Total the net profit/loss
+    total = sum(row[1],start=0)
+    print("Total: ", total)
+
+#Changes of Profit/Losses & Average
+   #loop through data
+   #row1[column1]=i
+   #i=0
+   #(sum i + (i+1))
+   # Differences = save that somewhere...
+   #loop through and then 
+   # Find Mean = Differences/rowcount
+ 
+
+#Greatest Increase
+    #look through Differences
+    #highest row = highest float
+    #print (#Greatest Increase in Profits: " highestrow)
+
+#Greatest Decrease
+    #look through differences
+    # lowestrow = smallest float
+    #print ("Greatest Decrease in Profits: ", lowestrow)
